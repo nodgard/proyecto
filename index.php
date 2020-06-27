cd9c00df702da2e4c3141ebeae328ec530db76f3
Nodgard <snodgard@gmail.com>
2020-06-27 11:53:35 -0500

    Mi primer commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TEST</title>
+	<link rel="stylesheet" type="text/css" href="index.css"/>
+	<script type="text/javascript" src="index.js"></script>
+</head>
+<body>
+	<h2>HOLA MUNDOx</h2>
+</body>
+</html>
